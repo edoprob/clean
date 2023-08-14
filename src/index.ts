7116c55f227ea768b1e3137416c3074b109485c3
edoprob <mauricio.prob@gmail.com>
2023-08-14 16:20:03 -0300

    testing 5

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 console.log('aaaaaaa')
-
 const message = 123213
 
 const messageText = 'hello world'
 
-if (message === 123213) {
-  console.log(messageText)
-};
+if (message === 123213) { console.log(messageText) };
