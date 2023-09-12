ec63f9a470426fa41f3cdf1b5419efa586f84e38
edoprob <mauricio.prob@gmail.com>
2023-09-12 13:30:38 -0300

    feat: ensure signup controller returns an error if no name has provider

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -3,6 +3,7 @@ import { SignUpController } from './signup'
 describe('SignUp Controller', () => {
   test('Should return 400 if no name has provided', () => {
     const sut = new SignUpController()
+
     const httpRequest = {
       body: {
         email: 'any_email',
@@ -13,5 +14,6 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
