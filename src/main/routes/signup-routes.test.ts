86a6492dac600468cebe47bc47ff3fc61fb7263f
edo <mauricio.prob@gmail.com>
2024-02-28 02:23:32 -0300

    refactor: connect to knex before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,22 @@
 import request from 'supertest'
 import app from '../config/app'
+import { config } from '../../database/knex'
 
 describe('SignUp Routes', () => {
+  const knex = config('test')
+
+  beforeAll(async () => {
+    await knex.migrate.latest()
+  })
+
+  beforeEach(async () => {
+    await knex('users').del()
+  })
+
+  afterAll(async () => {
+    await knex.destroy()
+  })
+
   test('Should return accaount on success', async () => {
     await request(app)
       .post('/api/signup')
