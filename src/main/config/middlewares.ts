856c5084eed732f19cea6ea00942919818f6f831
edo <mauricio.prob@gmail.com>
2024-02-28 01:19:10 -0300

    feat: ensure requests return json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import type { Express } from 'express'
 import { bodyParser } from '../middlawares/body-parser'
 import { cors } from '../middlawares/cors'
+import { contentType } from '../middlawares/content-type'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
