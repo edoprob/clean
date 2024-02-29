0296ce85402ba5b99ecdc92a4e4ca8c056fedac5
edoprob <mauricio.prob@gmail.com>
2024-02-29 18:07:33 -0300

    bug: SignUpController all bugs done, finally

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,28 +1,8 @@
 import type { Router } from 'express'
-// import { makeSignUpController } from '../factories/signup'
-// import { adaptRoute } from '../adapters/express-route-adapter'
+import asyncHandler from 'express-async-handler'
+import { makeSignUpController } from '../factories/signup'
+import { adaptRoute } from '../adapters/express-route-adapter'
 
 export default (router: Router): void => {
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  router.post('/signup', router.post('/signup', (req, res) => {
-    res.json({
-      id: 1,
-      name: 'a',
-      email: 'a',
-      password: 'a'
-    })
-  }))
+  router.post('/signup', asyncHandler(adaptRoute(makeSignUpController())))
 }
-/*
-
-  adaptRoute(makeSignUpController())
-
-  router.post('/signup', (req, res) => {
-    res.json({
-      id: 1,
-      name: 'a',
-      email: 'a',
-      password: 'a'
-    })
-  })
-*/
